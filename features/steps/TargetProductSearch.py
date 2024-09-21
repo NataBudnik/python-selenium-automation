c1a024ed65c869a315a632cd2beb1723ced2f311
Natalya <sofazonia@gmail.com>
2024-09-20 20:19:51 -0400

    Homework 4.1 added variable

--- features/steps/TargetProductSearch.py
+++ features/steps/TargetProductSearch.py
@@ -8,20 +8,18 @@ def open_main(context):
     context.driver.get('https://www.target.com/')
 
 
-@when('Search for a product')
-def search_product(context):
-    # print(item)
-    # Search field => enter tea
-    context.driver.find_element(By.ID, 'search').send_keys('tea')
-    # Search button => click
-    context.driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']").click()
-    sleep(5)
+@when('Search for a product {product_name}')
+def search_product(context, product_name):
+  context.driver.find_element(By.ID, 'search').send_keys(product_name)
+  context.driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']").click()
+
+sleep(4)
 
 
-@then('Verify that correct search results shown')
-def verify_results(context):
-    actual_result = context.driver.find_element(By.XPATH, "//div[@data-test='resultsHeading']").text
-    expected_result = 'tea'
-    assert expected_result in actual_result, f'Expected {expected_result}, got actual {actual_result}'
+@then('Verify that correct search results shown for {product_name}')
+def verify_results(context, product_name):
+  actual_result = context.driver.find_element(By.XPATH, "//div[@data-test='resultsHeading']").text
+  expected_result = product_name
+  assert expected_result in actual_result, f'Expected {expected_result}, got actual {actual_result}'
 
-    sleep(4)
+sleep(4)
